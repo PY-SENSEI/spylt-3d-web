8bfe7f33dcd0abb7e49578f988b92fb4d7400914
PY-SENSEI <gauravjena48@gmail.com>
2026-01-08 16:26:59 +0530

    flavour slider

--- src/components/FlavorTitle.jsx
+++ src/components/FlavorTitle.jsx
@@ -1,6 +1,23 @@
 const FlavorTitle = () => {
   return (
-    <div>FlavorTitle</div>
+    <div
+      className="general-title col-center h-full 2xl:gap-32 xl:gap-24
+    gap-16"
+    >
+        <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
+           <h1>We have 6</h1>
+        </div>
+
+        <div style={{}} className="flavor-text-scroll">
+              <div className="bg-mid-brown pb-5 2xl:pt-0 pt-3 2xl:px-5 px-3">
+                  <h2 className="text-milk">freaking</h2>
+              </div>
+        </div>
+
+        <div className="overflow-hidden 2xl:py-0 py-3 second-text-split">
+             <h1>Delicious flowers</h1>
+        </div>
+    </div>
   )
 }
 
--- src/sections/FlavorSection.jsx
+++ src/sections/FlavorSection.jsx
@@ -1,11 +1,16 @@
+import FlavorSlider from "../components/FlavorSlider"
+import FlavorTitle from "../components/FlavorTitle"
+
 const FlavorSection = () => {
   return (
     <section className="flavor-section">
-       <div className="h-full flex lg:flex-row items-center relative">
+       <div className="h-full flex lg:flex-row flex-col items-center relative">
          <div className="lg:w-[57%] flex-none h-80 lg:h-full md:mt-20xl xl:mt-0">
-             <FlavorSection />
+             <FlavorTitle />
          </div>
-         <div></div>
+         <div className="h-full">
+          <FlavorSlider />
+         </div>
        </div>
     </section>
   )
